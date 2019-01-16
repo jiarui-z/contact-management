cf4316af486587db2dcb1e6a2b0582d7cdc71d36
Jiarui Zhang <jrzhang9912@gmail.com>
2019-01-15 23:01:30 -0500

    final-v

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">
+        <span className="text-primary">About</span> Contact Manager
+      </h1>
+      <p className="lead">Simple application to manage contacts</p>
+    </div>
+  );
+};
